77f8cb045c956829d4182d4ac1f2783f6b213913
Darren Haken <darrenhaken@thoughtworks.com>
2016-11-27 13:38:06 -0800

    Add procfile and changed JAR name in project.clj

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject todo-backend-clojure "0.1.0-SNAPSHOT"
+(defproject todo-backend-clojure "1.0.0-SNAPSHOT"
   :description "a compojure implementation of https://github.com/moredip/todo-backend"
   :url "https://github.com/darrenhaken/todo-backend-clojure"
   :min-lein-version "2.0.0"
   :aot :all
   :java-target "1.8"
-  :uberjar-name "todo-backend-clojure-standalone.jar"
+  :uberjar-name "todo-backend-standalone.jar"
   :resource-paths ["resources"]
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.json "0.2.6"]
